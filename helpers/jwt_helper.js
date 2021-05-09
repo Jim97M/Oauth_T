4e9244c2181936b0e7a049197b886654b462be09
Jim97M <wabuyajames@gmail.com>
2021-05-09 05:18:43 +0300

    JsonWebToken Updated

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -6,9 +6,10 @@ module.exports = {
         return new Promise((resolve, reject) => {
 
             const payload = {
+
             }
 
-            const secret = process.env.ACCESS_TOKEN_SECRET
+             const secret = process.env.ACCESS_TOKEN_SECRET
 
             const options = {
 
@@ -18,9 +19,37 @@ module.exports = {
             }
 
             JWT.sign(payload, secret, options, (err, token) => {
-                if(err) reject(err)
+                if(err) {
+                   //reject(err)
+                    console.log(err.message)
+                    reject(createError.InternalServerError())
+                } 
                 resolve(token)
             })
         })
-    }
+    },
+
+    //Create a verify access token function for only authenticated users
+      verifyAccessToken: (req,res,next) => {
+          //Check if the authorization headers exist in request
+          if(!req.headers['authorization']) return next(createError.Unauthorized())
+
+          //If the header is present
+          const authHeader = req.headers['authorization']
+
+          //Store the authorization header by split in an array
+          const bearerToken = authHeader.split(' ')
+
+          const token = bearerToken[1]
+
+          JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+              if(err) {
+                  return next(createError.Unauthorized())
+              }
+
+              req.payload = payload
+
+              next()
+          })
+      }
 }
